8cb0d00e6d2e8a7270f0cb18deb54d8aa6755d6b
Kakalashvili-jpg <ddato0365@gmail.com>
2025-05-09 00:05:32 -0700

    samo

---
+++ level 2/homework.py
@@ -0,0 +1,33 @@
+# კონკატინაცია არის სტრინგების შეტყუპება
+
+print("aleqsisi" + "giorg")
+print("maria" + "nika")
+print("nica" + "saba")
+
+# + (მიმატება)
+# - (გამოკლება)
+# * (გამრავლება)
+# / (გაყოფა)
+
+num1 =  21
+num2 = 7
+
+print( num1 + num2 )
+print( num1 - num2 )
+print( num1 * num2 )
+print( num1 / num2 )
+
+name = "daviti"
+print("daviti" * 11)  # < ---- (davitidavitidavitdavitidavitidavitidavitidavitidavitidavitidaviti)
+
+name = "daviti"
+surname = "kakalashvili"
+print("daviti" + "kakalashvili")
+
+print("davitkakalashvili" * 100)
+
+# city => "London':
+# print"city)
+
+city =  "london"
+print(city)
---
+++ level 2/python.py
@@ -0,0 +1,22 @@
+#cvladi aris yuti sadac vinaxavt monacemebs
+
+num1 = 70
+num2 = 7
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+name = "davit"
+age = 12
+
+print("davit" * 12)
+
+
+name = "davit"
+surname = "kakalashvili"
+age = 12
+height = 1.65
+
+print("gamarjoba chemi saxelia",name,"chemi gvaria",surname,"chemi asakia",age,"chemi simaglea",height)
