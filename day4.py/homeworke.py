e55fd5ed359ecd4bb826c0bc9619100f767ac682
Kakalashvili-jpg <ddato0365@gmail.com>
2025-05-23 06:18:05 -0700

    daviti

---
+++ day4.py/homeworke.py
@@ -0,0 +1,23 @@
+#davakeba 1
+name = "daviti"
+name = " giorgi"
+print(name) 
+
+#davaleba 2
+num1 = 70 
+num2 = 7
+print(num1 + num2)
+print(num1 - num2)
+print(num1 / num2)
+print(num1 * num2)
+
+#davaleba 3
+num = 7
+print(num + 1)
+
+#davaleba 4
+#ცვლადების განახლება არის სასარგებლო რადგან თუ რაიმე ცვლადის განახლება გვინდა და გვეცოდინება მარტივი ხერხი თუ როგორ გავანახლოთ ცვლადი მაშინ აღარ მოგვიწევს ბევრი კოდის წერა
+#მაგალითი; 
+#num=14 
+#num=31
+#print(num)
