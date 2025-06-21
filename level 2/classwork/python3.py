8ed8a8a2510e1db473534f8cb409a76e4a3005b2
Kakalashvili-jpg <ddato0365@gmail.com>
2025-06-21 13:47:34 +0400

    daviti

---
+++ level 2/classwork/python3.py
@@ -0,0 +1,7 @@
+
+# 4)შექმენით 4 ცვლადი (პირველში შეინახეთ თქვენი სახელი, მეორეში შეინახეთ თქვენი გვარი, მესამეში თქვენი ასაკი და მეოთხეში თქვენი სიმაღლე) შემდეგ ამ ცვლადების გამოყენებით დაბეჭდეთ ერთი დიდი წინადადება
+name = "daviti"
+surname = "kakalashvili"
+age = 12
+height = 1.67
+print("chemi saxelia" ,name , "chemi gavria" ,surname , "chemi asakia" ,age , "chemi sigamlea" ,height)
